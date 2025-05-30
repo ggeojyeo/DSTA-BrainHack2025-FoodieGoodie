ec2ee444a946949660241ea2c5ebec5c568f1609
Adolphus Chua <github.adolphus@gmail.com>
2025-05-30 16:40:42 +0800

    Initial Commit for backend

---
+++ backend/controllers/userController.js
@@ -0,0 +1,20 @@
+// controllers/userController.js
+const User = require('../models/User');
+
+exports.createUser = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json(user);
+    } catch (err) {
+        res.status(400).json({ error: err.message });
+    }
+};
+
+exports.getUsers = async (req, res) => {
+    try {
+        const users = await User.find();
+        res.json(users);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+};
