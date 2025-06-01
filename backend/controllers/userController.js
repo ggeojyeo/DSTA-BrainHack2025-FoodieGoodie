4ab8be25c42c9234f402e8af53c13a98f1405d16
Liew Jia Wei <mjw.liew@gmail.com>
2025-06-01 11:18:50 +0800

    login route, not tested

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 // controllers/userController.js
 const User = require('../models/User');
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = ""
 
 exports.createUser = async (req, res) => {
     try {
@@ -18,3 +20,25 @@ exports.getUsers = async (req, res) => {
         res.status(500).json({ error: err.message });
     }
 };
+
+exports.loginUser = async (req, res) => {
+    const {email, password} = req.body;
+
+    try {
+        const user = await User.findOne({email});
+
+        if (!user || user.password !== password) {
+            return res.status(401).json({error: "Invalid credentials"});
+        }
+
+        const token = jwt.sign (
+            {userId: user._id},
+            JWT_SECRET,
+            {expiresIn: "12h"}
+        );
+
+        res.json({token});
+    } catch (err) {
+        res.status(500).json({error: "Server error"})
+    }
+}
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -3,7 +3,8 @@ const express = require('express');
 const router = express.Router();
 const { createUser, getUsers } = require('../controllers/userController');
 
-router.post('/', createUser);
+router.post('/register', createUser);
 router.get('/', getUsers);
+router.post('/login', loginUser);
 
 module.exports = router;
